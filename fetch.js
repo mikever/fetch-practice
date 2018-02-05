5fb6c8cbd9d6caa302c05643d15ced902e8cdd6a
Michael <mveronie@gmail.com>
2018-02-05 06:30:49 -0500

    Static file places in fetch folder

--- fetch.js
+++ fetch.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('/home/mike/Documents/projects/develop-practice/fetch-practice'));
+app.use(express.static('fetch'));
 
 app.listen(8080, () => {
   console.log('App listening on Port 8080');
